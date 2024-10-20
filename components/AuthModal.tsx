ec634e0fd0b7b2e77bbfc65eedae5f857660c36f
JarvisChan666 <744765386@qq.com>
2024-10-20 15:23:33 +0800

    1122151

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -5,7 +5,7 @@ import {
   useSupabaseClient,
 } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/navigation";
-import {Auth} from "@supabase/auth-ui-react"
+import { Auth } from "@supabase/auth-ui-react"
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 import { Modal } from "./modal";
@@ -16,44 +16,44 @@ export function AuthModal() {
   const supabaseClient = useSupabaseClient();
   const router = useRouter();
   const { session } = useSessionContext();
-  const {onClose, isOpen} = useAuthModal();
+  const { onClose, isOpen } = useAuthModal();
 
   // When we login successfully, we will close the modal
   useEffect(() => {
     if (session) {
-        router.refresh();
-        onClose();
+      router.refresh();
+      onClose();
     }
   }, [session, router, onClose])
 
   const onChange = (open: boolean) => {
-    if(!open) {
-        onClose();
+    if (!open) {
+      onClose();
     }
   }
 
   return (
     <Modal
-      title="Happy birthday to Jelly Chen! ! !"
+      title={<span className="fancy-text">Happy birthday to Jelly Chen! ! !</span>}
       description="Login to your acocunt"
       isOpen={isOpen}
       onChange={onChange}
     >
-      <Auth 
-      theme="dark"
-      providers={["spotify", "google", "notion", "github"]}
-      supabaseClient={supabaseClient}
-      appearance={{
-        theme:ThemeSupa,
-        variables: {
+      <Auth
+        theme="dark"
+        providers={["spotify", "google", "notion", "github"]}
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
             default: {
-                colors: {
-                    brand: '#404040',
-                    brandAccent: '#f171b4'
-                }
+              colors: {
+                brand: '#404040',
+                brandAccent: '#f171b4'
+              }
             }
-        }
-      }}
+          }
+        }}
       />
     </Modal>
   );
